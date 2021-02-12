ba07a818b0969b0b0e2b20d9e5fe08754bf62361
onsjannet <onsjannet@gmail.com>
2021-02-12 08:12:08 +0100

    added mining difficulty to ctrl the timing / difficulty of the mining / adding new blocks to the blockchain

--- main.js
+++ main.js
@@ -14,18 +14,29 @@ class Block{
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = ''
+        this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     // calculates the hash of the block
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + this.nonce + JSON.stringify(this.data)).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock(){
@@ -38,7 +49,7 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -63,8 +74,12 @@ class Blockchain{
 
 // tests
 let SwiftieCoin = new Blockchain();
+
+console.log(" Mining Block 1...");
 SwiftieCoin.addBlock(new Block(1, "02/09/2021", {amount: 13}));
+
+console.log(" Mining Block 2...");
 SwiftieCoin.addBlock(new Block(2, "15/09/2021", {amount: 10}));
 
-console.log('Is blockchain valid? ' + SwiftieCoin.isChainValid());
-console.log(JSON.stringify(SwiftieCoin, null, 4));
+//console.log('Is blockchain valid? ' + SwiftieCoin.isChainValid());
+//console.log(JSON.stringify(SwiftieCoin, null, 4));
